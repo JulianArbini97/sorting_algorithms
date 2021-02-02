5e6f684628144482757be347e59d9b3f449daf5e
JulianArbini97 <1890@holbertonschool.com>
2021-02-02 17:55:25 +0000

    101

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+/**
+ * cocktail_sort_list - insertion sort algorithm
+ * @array: list
+ * @size: size
+ * Return: sorted array
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+    listint_t *temp;
+    listint_t *move;
+    listint_t *swap;
+
+    if (!list || *list == NULL)
+    {
+        return;
+    }
+
+    temp = (*list)->next;
+    while (temp != NULL)
+    {
+        move = *list;
+        while (move->next != NULL)
+        {
+            if (move->n > move->next->n)
+            {
+                swap = move;
+                move = move->next;
+                move->next = swap;
+                *list = move;
+                print_list(*list);
+            }
+            else
+            {
+                move = move->next;
+            }
+        }
+
+        while (move->prev != NULL)
+        {
+            if (move->n > move->prev->n)
+            {
+                swap = move;
+                move = move->prev;
+                move->prev = swap;
+                print_list(*list);
+            }
+            else
+            {
+                move = move->prev;
+            }
+        }
+        temp = temp->next;
+    }
+}
